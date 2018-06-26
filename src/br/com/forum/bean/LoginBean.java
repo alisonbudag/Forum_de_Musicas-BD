2c7f0186f165965b99e48cb1538575a0bc982792
66025 <66025@SALA412.proway.treina>
2018-06-26 20:33:13 -0300

    dadads

---
+++ src/br/com/forum/bean/LoginBean.java
@@ -0,0 +1,31 @@
+package br.com.forum.bean;
+
+public class LoginBean {
+	
+	//Atributos
+	private int idLogin;
+	private String user;
+	private String password;
+	
+	//Getters and Setters
+	public int getIdLogin() {
+		return idLogin;
+	}
+	public void setIdLogin(int idLogin) {
+		this.idLogin = idLogin;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
